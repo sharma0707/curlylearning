62292e0a8a0ee665ed99fd1d195b877a4962b166
unknown <sonu813028@gmail.com>
2021-03-25 23:07:29 +0530

    message

--- backend.java
+++ backend.java
@@ -4,12 +4,16 @@ class backend{
     
     public static void main(String...)throws IOException, SQLException{
         try{
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection con = DriverManager.getConnection("")
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc.mysql://localhost:3306/users","root", "1234");
             Statement stmt = con.createStatment();
-            ResultSet rs = stmt.executeQuery("select *from student");
+            ResultSet rs = stmt.executeQuery("select *from students");
             while(rs.next())
-
+            {
+                System.out.println("rs.getInt(1)+ " "+rs.getString(2)");
+            }
+            
+            con.close();
         }
         catch(Exception e){System.out.println("e");}
 
